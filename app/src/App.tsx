e887d8d850f38fe3628b81f06c0682027d418ec7
Hemal99 <h.githmin@gmail.com>
2025-04-08 15:15:46 +0530

    fix: update header title and button text in App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,10 +16,10 @@ export const App = () => {
     <div className="App">
       <div className="header">
         <div>
-          <h1>Play 2048 Hello</h1>
+          <h1>Play 2048 Hello c</h1>
         </div>
         <div>
-          <Button onClick={handleRestart}>Restart Hello</Button>
+          <Button onClick={handleRestart}>Restart Hello updated</Button>
         </div>
       </div>
       <Game key={date.toISOString()} />
